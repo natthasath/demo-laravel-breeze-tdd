0b087eb6b3bfa3962c40fa6fe19160e517554e76
natthasath <natthasath.sak@gmail.com>
2024-06-20 13:50:04 +0700

    Add Factories

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create 10 fake users
+        User::factory()->count(10)->create();
+    }
+}
